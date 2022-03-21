658286bd2c78129e18bf8f2e02b642183e071c99
Chupexy <osaretin520@gmail.com>
2022-03-21 15:04:36 +0100

    made changes to index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const http = require('http');
- 
-const HOSTNAME= '127.0.0.1'
-const PORT = 2900
+const HOSTNAME = '127.0.0.1'
+const PORT = 4500
 
-const server = http.createServer((req,res) =>{
-    console.log('here');
-    res.statusCode= 200
-    //res.setHeader('Content-type', 'text/plain')
-    res.write('Hello Viewers')
-    res.end
+const server= http.createServer((req,res) =>{
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/plain')
+    res.end('Hello World')
 })
 
 server.listen(PORT,HOSTNAME =>{
-    console.log('server listening at the usual address')
+    console.log('server running at local address')
 })
