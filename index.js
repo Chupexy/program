584fa83d02a80c0948fd45882cd4292cbb2fd9fc
Chupexy <osaretin520@gmail.com>
2022-03-22 12:14:36 +0100

    added some code

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const http = require('http');
-const HOSTNAME = '127.0.0.1'
-const PORT = 4500
+const path = require('path');
+const fs = require('fs');
 
 const server= http.createServer((req,res) =>{
-    res.statusCode = 200
-    res.setHeader('Content-type', 'text/plain')
-    res.end('Hello World')
-})
+   let filepath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+   let extname = path.extname(filepath);
+   let contentType = 'text/html'
 
-server.listen(PORT,HOSTNAME =>{
-    console.log('server running at local address')
-})
+   switch(extname){
+       case '.js':
+           contentType= 'text/javascript';
+           break;
+       case '.css':
+           contentType= 'text/css';
+           break;
+       case '.json':
+           contentType= 'application/json';
+           break;
+       case '.png':
+           contentType= 'image/png';
+           break;
+       case '.jpg':
+           contentType= 'image/jpg';
+           break;
+
+   }
+   fs.readFile(filepath, (err, content) =>{
+       if(err){
+           if(err.code == 'ENOENT'){
+               fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) =>{
+                   res.writeHead(200, {'Content-Type': 'text/html'});
+                   res.end(content, 'utf8');
+               });
+           }else{
+               res.writeHead(500);
+               res.end(`Server Error: ${err.code}`);
+           }
+       }else{
+           res.writeHead(200, {'Content-Type': contentType});
+           res.end(content, 'utf8');
+       }
+   });
+});
+const PORT =process.env.PORT || 5000;
+server.listen(PORT,  ()=>
+    console.log('server running at local address'));
